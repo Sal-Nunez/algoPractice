f3005b31c1d4c99d57392dcf8da9df2654811858
Sal-Nunez <88513143+Sal-Nunez@users.noreply.github.com>
2021-08-21 17:37:32 -0700

    algoPractice

---
+++ alwaysHungry.js
@@ -0,0 +1,17 @@
+function alwaysHungry(arr) {
+    // your code here 
+    var hasEaten = 0
+    for(i = 0; i < arr.length; i++) {
+        if(arr[i] == "food") {
+            console.log("yummy");
+            var hasEaten = hasEaten + 1;
+        }
+    }
+    if(hasEaten == 0) {
+        console.log("I'm Hungry")
+    }
+}
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"
